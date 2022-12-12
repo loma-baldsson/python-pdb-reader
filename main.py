458393b035b41f749d98d65de61fb69357aa9254
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-11 21:01:36 -0800

    Edit `main.py` to test `doc()` and `NullTerminatedString()`
    just me messing around

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import pprint
 
 from pdbheader import PDBHeader
 from pdbconvert import palm_to_datetime, datetime_to_palm
+from pdbtypes.null_terminated_string import NullTerminatedString
+from pdbtypes.doc import doc
+
+
+class Test:
+    def __init__(self):
+        self._name = b""
+
+    @doc(NullTerminatedString, "_name", 32)
+    def item(self):
+        """Hello"""
 
 
 def main():
-    filename = sys.argv[1] if len(sys.argv) > 1 else "./test/test.pdb"
-    with open(filename, "rb") as f:
-        header = PDBHeader.load_from_file(f)
-
-    print(header)
+    test = Test()
+    print(test.item)
+    test.item = "Hello"
+    print(test.item)
+    # filename = sys.argv[1] if len(sys.argv) > 1 else "./test/test.pdb"
+    # with open(filename, "rb") as f:
+    #     header = PDBHeader.load_from_file(f)
+    #
+    # print(header)
 
     # with open(filename, "wb") as f:
     #     header.name = "edited"
