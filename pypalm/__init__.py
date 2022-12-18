16515fedacf4d74ecb5f5e72b0952cd60ffcb975
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-17 21:22:09 -0800

    Refactor pdbheader.py to use new PDBType classes

--- pypalm/__init__.py
+++ pypalm/__init__.py
@@ -7,3 +7,6 @@ from .pdbtype.null_terminated_string import *
 from .pdbtype.pdbtype_base import *
 from .pdbtype.pdbtypes_template import *
 from .pdbtype.palm_time import *
+
+# put at end as a band-aid fix for circular imports
+from .pdbheader.pdbheader import *
---
+++ pypalm/pdbheader/pdbheader.py
@@ -0,0 +1,45 @@
+from .. import NullTerminatedString, FixedLengthString, Word, DWord, PalmTime, template_wrapper
+
+
+@template_wrapper
+class PDBHeader:
+    def __init__(self):
+        self._items = {
+            "name": NullTerminatedString(32),
+            "attributes": Word(),
+            "version": Word(),
+            "creation_time": PalmTime(),
+            "modification_time": PalmTime(),
+            "backup_time": PalmTime(),
+            "modification_number": DWord(),
+            "app_info_offset": DWord(),
+            "sort_info_offset": DWord(),
+            "type": FixedLengthString(4),
+            "creator": FixedLengthString(4),
+            "unique_id": DWord(),
+            "next_record_id": DWord(),
+            "num_records": Word(),
+        }
+
+    @classmethod
+    def load_from_file(cls, file):
+        self = cls()
+
+        for item in self._items.values():
+            item_size = len(item.getter_bytes())
+            item.setter_bytes(file.read(item_size))
+
+        return self
+
+    def write_to_file(self, file):
+        for item in self._items.values():
+            file.write(item.getter_bytes())
+
+    def __str__(self):
+        out = ""
+
+        for key, item in self._items.items():
+            val = item.getter()
+            out += f"{key}: {val!r}\n"
+
+        return out
