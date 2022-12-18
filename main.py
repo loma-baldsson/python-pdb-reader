deb522faffc01c7e0fe50a2ed38e51291904e79b
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-17 19:08:59 -0800

    Update main.py
    Tests the new system and stuff!

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import datetime
-import sys
-import pprint
 
-from pdbheader import PDBHeader
-from pdbconvert import palm_to_datetime, datetime_to_palm
-from pdbtypes import pdbtype, FixedLengthString
-from pypalm.pdbtypes import FixedLengthStringBytes
+from pypalm import FixedLengthString, NullTerminatedString, BigEndianInt, template_wrapper, PalmTime
 
 
+@template_wrapper
 class Test:
     def __init__(self):
-        self._x = b""
-
-    @pdbtype(FixedLengthString(5), "_x")
-    def x(self):
-        """Hello"""
-
-    @pdbtype(FixedLengthStringBytes(5), "_x")
-    def x_bytes(self):
-        """Bytes of x"""
+        self._items = {
+            "x": FixedLengthString(16),
+            "y": NullTerminatedString(16),
+            "z": BigEndianInt(2),
+            "date": PalmTime()
+        }
 
 
 def main():
     test = Test()
+
     print(test.x)
-    test.x = "Hello"
+    print(test.x_bytes.hex(" "))
+    test.x = "HelloHelloHelloH"
     print(test.x)
-    print(test.x_bytes)
-    # filename = sys.argv[1] if len(sys.argv) > 1 else "./test/test.pdb"
+    print(test.x_bytes.hex(" "))
+
+    print(test.y)
+    test.y = "Hello, World!"
+    print(test.y)
+    print(len(test.y_bytes))
+
+    test.z = 20
+    print(test.z)
+    print(test.z_bytes)
+
+    print(test.date)
+    test.date = datetime.datetime.now()
+    print(test.date)    # filename = sys.argv[1] if len(sys.argv) > 1 else "./test/test.pdb"
     # with open(filename, "rb") as f:
     #     header = PDBHeader.load_from_file(f)
     #
