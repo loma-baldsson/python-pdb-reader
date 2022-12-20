3152a1a62061d8a68fa49bff1eb03a016ff6581d
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-19 21:18:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 
 from pypalm.pdbtype import FixedLengthString, NullTerminatedString, BigEndianInt, PalmTime
@@ -6,46 +7,53 @@ from pypalm.pdbtemplates import Template
 
 
 class Test(Template):
-    def __init__(self):
-        self._items = {
+    def __init__(self, **kwargs):
+        items = {
             "x": FixedLengthString(16),
             "y": NullTerminatedString(16),
             "z": BigEndianInt(2),
             "date": PalmTime()
         }
 
+        super().__init__(items, **kwargs)
+
+
+def test_template():
+    test = Test()
+
+    print(test.x)
+    print(test.x_bytes.hex(" "))
+    test.x = "HelloHelloHelloH"
+    print(test.x)
+    print(test.x_bytes.hex(" "))
+
+    print(test.y)
+    test.y = "Hello, World!"
+    print(test.y)
+    print(len(test.y_bytes))
+
+    test.z = 20
+    print(test.z)
+    print(test.z_bytes)
+
+    print(test.date)
+    test.date = datetime.datetime.now()
+    print(test.date)
+
 
 def main():
-    # test = Test()
-    #
-    # print(test.x)
-    # print(test.x_bytes.hex(" "))
-    # test.x = "HelloHelloHelloH"
-    # print(test.x)
-    # print(test.x_bytes.hex(" "))
-    #
-    # print(test.y)
-    # test.y = "Hello, World!"
-    # print(test.y)
-    # print(len(test.y_bytes))
-    #
-    # test.z = 20
-    # print(test.z)
-    # print(test.z_bytes)
-    #
-    # print(test.date)
-    # test.date = datetime.datetime.now()
-    # print(test.date)
-
-    filename = sys.argv[1] if len(sys.argv) > 1 else "./test/test.pdb"
+    filename = sys.argv[1] if len(sys.argv) > 1 else "./test_pdbs/test.pdb"
     with open(filename, "rb") as f:
         header = PDBHeader.load_from_file(f)
 
-    record_headers = []
-    for i in range(header.num_records):
-        record_headers.append(RecordHeader.load_from_file(f))
+        record_headers = []
+        for i in range(header.num_records):
+            record_headers.append(RecordHeader.load_from_file(f))
 
-    print(header)
+    new_header = PDBHeader(name="Hello")
+
+    print(new_header)
+    print(repr(header), "\n")
     print(record_headers)
 
 
