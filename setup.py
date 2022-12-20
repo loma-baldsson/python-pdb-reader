c0997e0688ed069a033b82400505ff5bd5bbba28
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-20 14:24:43 -0800

    Remove comments in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 # template from pypa's sample project
 # https://raw.githubusercontent.com/pypa/sampleproject/db5806e0a3204034c51b1c00dde7d5eb3fa2532e/setup.py
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-# Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
 
 setup(
     name="pypalm",
-    version="0.0.1a1",  # Required
+    version="0.0.1a1",
     description="A (currently unfinished) python interface for the Palm DB format.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/loma-baldsson/python-pdb-reader",
     author="loma-baldsson",
     author_email="howtopoopin2021official@gmail.com",
-    classifiers=[  # Optional
+    classifiers=[
         "Development Status :: 3 - Alpha",
 
         "Intended Audience :: Developers",
@@ -39,7 +34,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.10, <4",
-    extras_require={  # Optional
+    extras_require={
         "test": ["pytest"],
     },
     project_urls={
