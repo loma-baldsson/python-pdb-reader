74466c0d5713bbb94e61162da8215774a2f1a6bb
loma-baldsson <102317848+loma-baldsson@users.noreply.github.com>
2022-12-20 16:52:23 -0800

    Separate `PalmTime()` to not depend on `BigEndianInt()`
    thanks mypy for showing my the liskov substitution principle

--- src/pypalm/pdbtype/palm_time.py
+++ src/pypalm/pdbtype/palm_time.py
@@ -1,20 +1,25 @@
 from datetime import datetime
 
-from . import DWord
+from . import PDBTypeBase
 from .. import palm_to_datetime, datetime_to_palm
 
 
-class PalmTime(DWord):
+class PalmTime(PDBTypeBase):
+    def __init__(self):
+        # palmtime objects are always 4 bytes
+        super().__init__(4)
+
     def get_default(self) -> datetime:
         # just an arbitrary date
         return datetime(2020, 1, 1)
 
     def getter(self) -> datetime:
-        timestamp = super().getter()
+        timestamp = int.from_bytes(self._value, "big")
         return palm_to_datetime(timestamp)
 
     def setter(self, value: datetime) -> None:
         # TODO: Warn user if their is chance of possible internal confusion between palm time and unix time
         assert isinstance(value, datetime)
 
-        super().setter(datetime_to_palm(value))
+        timestamp = datetime_to_palm(value)
+        self._value = timestamp.to_bytes(self._size, "big")
